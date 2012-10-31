92e2372bac1f10e08149b46f3b2194ed61aa3a57
baptistejesse <baptistejesse@yahoo.com>
2012-10-31 13:10:51 -0400

    again

--- app.rb
+++ app.rb
@@ -12,8 +12,11 @@ end
 get '/' do
   
  response = Twilio::TwiML::Response.new do |r|
+    r.Pause :length=>"5"
     r.Say 'Welcome to Daycare #1', :voice => 'woman'
-    r.Gather :numDigits=>"1", :action=>"http://sleepy-river-3449.herokuapp.com/jesse"
+    if params[:Digits] = 123
+    r.Gather :action=>"http://sleepy-river-3449.herokuapp.com/jesse", :method => "GET"
+  end
  end  
 
 response.text
