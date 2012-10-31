ce9070661e4753ec560ee64f854d6a1f9ec99c47
baptistejesse <baptistejesse@yahoo.com>
2012-10-31 18:11:50 -0400

    again

--- app.rb
+++ app.rb
@@ -12,10 +12,10 @@ end
 get '/' do
   
  response = Twilio::TwiML::Response.new do |r|
-    r.Pause :length=>"2"
-    r.Say 'Welcome to my Conference call to enter please press 1 2 3', :voice => 'woman', :language => 'en'
+    r.Pause :length=>"1"
+    r.Say ' Welcome  to  my  Conference call to enter please press 1  2  3 #', :voice => 'woman', :language => 'en'
     if params[:Digits] = "123"
-    r.Gather :action=>"http://sleepy-river-3449.herokuapp.com/jesse", :method => "GET"
+    r.Gather :action=>"/jesse", :method => "GET"
   end
  end  
 
@@ -25,6 +25,7 @@ end
 
 get '/jesse'  do
   response = Twilio::TwiML::Response.new do |r|
+   r.Say "Connecting  you  to  the  conference", :voice => 'woman', :language => 'en'
    r.Dial do |d|
     d.Conference "RoomPop", :waitUrl=>"http://twimlets.com/holdmusic?Bucket=com.twilio.music.classical" ,:startConferenceOnEnter=>"true", :endConferenceOnExit=>"true"
     end
