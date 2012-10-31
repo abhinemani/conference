ab45aa6ea7d5bfd5a198dbb7ba9000e8d597681e
baptistejesse <baptistejesse@yahoo.com>
2012-10-31 12:29:43 -0400

    again

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :gemcutter
+gem 'sinatra', '1.3.0'
+gem 'thin', '1.2.7'
+gem 'json'
+gem 'twiliolib'
+gem 'twilio-ruby'
+gem 'httparty'
---
+++ app.rb
@@ -0,0 +1,32 @@
+require 'sinatra'
+require 'rubygems'
+require 'httparty'
+require 'twiliolib'
+require 'twilio-ruby'
+
+before do
+content_type :xml
+end
+
+
+get '/' do
+  
+ response = Twilio::TwiML::Response.new do |r|
+    r.Say 'Welcome to Daycare #1', :voice => 'woman'
+    r.Gather :numDigits=>"1", :action=>"/jesse"
+ end  
+
+response.text
+end  
+  
+
+get '/jesse'  do
+  response = Twilio::TwiML::Response.new do |r|
+   r.Dial do |d|
+    d.Number "+13476108367"
+   end
+  end
+
+response.text
+
+end
