045520240bf46a4d9e503f6c7d739515f306c079
baptistejesse <baptistejesse@yahoo.com>
2012-10-31 15:32:04 -0400

    again

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ get '/' do
  response = Twilio::TwiML::Response.new do |r|
     r.Pause :length=>"5"
     r.Say 'Welcome to Daycare #1', :voice => 'woman'
-    if params[:Digits] = 123
+    if params[:Digits] = "123"
     r.Gather :action=>"http://sleepy-river-3449.herokuapp.com/jesse", :method => "GET"
   end
  end  
@@ -25,9 +25,10 @@ end
 
 get '/jesse'  do
   response = Twilio::TwiML::Response.new do |r|
-   r.Dial do |d|
+   r.Dial :callerId =>"+13474620270" do |d|
     d.Number "+13476108367"
-   end
+    end
+   r.say "Connected", :voice => 'connected'
   end
 
 response.text
