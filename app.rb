c8587eac7611e2cd7d0fc59c7596b9ca08842283
Abhi <abhi.nemani@gmail.com>
2013-11-20 21:38:55 -0800

    updated config file

--- app.rb
+++ app.rb
@@ -1,37 +1,10 @@
-require 'sinatra'
 require 'rubygems'
-require 'httparty'
-require 'twiliolib'
-require 'twilio-ruby'
-
-before do
-content_type :xml
-end
-
-
-get '/' do
-  
- response = Twilio::TwiML::Response.new do |r|
-    r.Pause :length=>"1"
-    r.Say ' Welcome  to  my  Conference call to enter please press 1  2  3 #', :voice => 'woman', :language => 'en'
-    if params[:Digits] = "123"
-    r.Gather :action=>"/abhi", :method => "GET"
-  end
- end  
-
-response.text
-end  
-  
-
-get '/abhi'  do
-  response = Twilio::TwiML::Response.new do |r|
-   r.Say "Connecting  you  to  the  conference", :voice => 'woman', :language => 'en'
-   r.Dial do |d|
-    d.Conference "RoomPop", :waitUrl=>"http://twimlets.com/holdmusic?Bucket=com.twilio.music.classical" ,:startConferenceOnEnter=>"true", :endConferenceOnExit=>"true"
-    end
-  
-  end
-
-response.text
-
+require 'sinatra'
+ 
+get '/hello' do
+  response =<<EOF
+<Response>
+  <Say>Hello from moocode</Say>
+</Response>
+EOF
 end
